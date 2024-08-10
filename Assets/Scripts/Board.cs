e65951fbc577d7b3875b3be1f8c980593d25c6a8
[Alisa Khrebtovich] <Mio.reepy@gmail.com>
2024-08-10 20:54:24 +0300

    Fix Method - GetConnectedTiles

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -33,44 +33,19 @@ public class Board : MonoBehaviour
             }
         }
     }
-    
-    private void Update()
-    {
-        if (!Input.GetKeyDown(KeyCode.A)) return;
-
-        foreach (var row in Tiles)
-        {
-            foreach (var connectedTiles in row[0].GetConnectedTiles())
-            {
-                connectedTiles.Icon.transform.DOScale(1.25f, _tweenDuration).Play();
-            }
-        }
-    }
 
     public async void Select(Tile tile)
     {
         if (!_selection.Contains(tile))
         {
             _selection.Add(tile);
-            Debug.Log($"Добавлено: {tile.x}, {tile.y}");
         }
         
         if (_selection.Count < 2) return;
 
         await Swap(_selection[0], _selection[1]);
         
-        Debug.Log($"Свайп: {_selection[0].x}, {_selection[0].y} и {_selection[1].x}, {_selection[1].y}");
-        
         _selection.Clear();
-
-        if (_selection.Count > 0)
-        {
-            Debug.Log($"Грязно");
-        }
-        else
-        {
-            Debug.Log($"Чисто");
-        }
     }
 
     public async Task Swap(Tile tile1, Tile tile2)
