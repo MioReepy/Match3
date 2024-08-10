81a57f098b631f8e92f793bd3c9bdc3dce2de6dc
[Alisa Khrebtovich] <Mio.reepy@gmail.com>
2024-08-10 18:42:09 +0300

    Refractoring

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 
 public class Board : MonoBehaviour
@@ -7,45 +9,103 @@ public class Board : MonoBehaviour
     public Tile[][] Tiles { get; private set; }
 
     private readonly List<Tile> _selection = new List<Tile>();
-
+    private const float _tweenDuration = 0.25f;
     private void Awake() => Instance = this;
 
     private void Start()
     {
         Tiles = new Tile[gameObject.transform.childCount][];
 
-        for (int y = 0; y < transform.childCount; y++)
+        for (int x = 0; x < transform.childCount; x++)
         {
-            Tiles[y] = new Tile[transform.GetChild(y).GetComponent<Row>().Tiles.Count]; 
+            Tiles[x] = new Tile[transform.GetChild(x).GetComponent<Row>().Tiles.Count]; 
                 
-            for (int x = 0; x < Tiles[y].Length; x++)
+            for (int y = 0; y < Tiles[x].Length; y++)
             {
-                var title = transform.GetChild(y).GetComponent<Row>().Tiles[x];
+                var title = transform.GetChild(x).GetComponent<Row>().Tiles[y];
                 
                 title.Item = ItemDataBase.Items[Random.Range(0, ItemDataBase.Items.Length)];
                 
                 title.x = x;
                 title.y = y;
 
-                Tiles[y][x] = title;
-                Debug.Log($"{y}, {x}");
+                Tiles[x][y] = title;
+            }
+        }
+    }
+    
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.A)) return;
 
+        foreach (var row in Tiles)
+        {
+            foreach (var connectedTiles in row[0].GetConnectedTiles())
+            {
+                connectedTiles.Icon.transform.DOScale(1.25f, _tweenDuration).Play();
             }
         }
     }
 
-    public void Select(Tile tile)
+    public async void Select(Tile tile)
     {
         if (!_selection.Contains(tile))
         {
             _selection.Add(tile);
+            Debug.Log($"Добавлено: {tile.x}, {tile.y}");
         }
         
         if (_selection.Count < 2) return;
+
+        await Swap(_selection[0], _selection[1]);
         
-        Debug.Log($"Выбрана плитка ({_selection[0].x}, {_selection[0].y}) и ({_selection[1].x}, {_selection[1].y})");
+        Debug.Log($"Свайп: {_selection[0].x}, {_selection[0].y} и {_selection[1].x}, {_selection[1].y}");
         
         _selection.Clear();
+
+        if (_selection.Count > 0)
+        {
+            Debug.Log($"Грязно");
+        }
+        else
+        {
+            Debug.Log($"Чисто");
+        }
     }
+
+    public async Task Swap(Tile tile1, Tile tile2)
+    {
+        var icon1 = tile1.Icon;
+        var icon2 = tile2.Icon;
+
+        var icon1Transform = icon1.transform;
+        var icon2Transform = icon2.transform;
+
+        var sequence = DOTween.Sequence();
+        sequence.Join(icon1Transform.DOMove(icon2Transform.position, _tweenDuration))
+                .Join(icon2Transform.DOMove(icon1Transform.position, _tweenDuration));
+
+        await sequence.Play().AsyncWaitForCompletion();
+        
+        icon1Transform.SetParent(tile2.transform);
+        icon2Transform.SetParent(tile1.transform);
+
+        tile1.Icon = icon2;
+        tile2.Icon = icon1;
+
+        var tile1Item = tile1.Item;
+        tile1.Item = tile2.Item;
+        tile2.Item = tile1Item;
+    }
+
+    // private void CanPop()
+    // {
+    //     
+    // }
+    //
+    // private void Pop()
+    // {
+    //     
+    // }
 }
     
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +8,7 @@ public class Tile : MonoBehaviour
     public int y;
 
     private Item _item;
-    private Image Icon;
+    internal Image Icon;
     private Button ButtonTile;
     
     public Item Item
@@ -23,14 +23,50 @@ public class Tile : MonoBehaviour
         }
     }
 
+    public Tile Left => x > 0 ? Board.Instance.Tiles[x - 1] [y] : null;
+    public Tile Right => x < Board.Instance.Tiles[x].Length ? Board.Instance.Tiles[x + 1] [y] : null;
+    public Tile Top => y > 0 ? Board.Instance.Tiles[x] [y - 1] : null;
+    public Tile Botton => y < Board.Instance.Tiles.Length ? Board.Instance.Tiles[x] [y + 1] : null;
+
+    public Tile[] Neighbours => new[]
+    {
+        Left,
+        Right,
+        Top,
+        Botton
+    };
+
     private void Awake()
     {
-        Icon = gameObject.GetComponent<Image>();
-        ButtonTile = gameObject.GetComponent<Button>();
+        Icon = gameObject.transform.GetChild(0).GetComponent<Image>();
+        ButtonTile = gameObject.transform.GetChild(0).GetComponent<Button>();
     }
 
     private void Start()
     {
          ButtonTile.onClick.AddListener(() => Board.Instance.Select(this));
+    }
+
+    public List<Tile> GetConnectedTiles(List<Tile> exluse = null)
+    {
+        var result = new List<Tile> { this };
+
+        if (exluse == null)
+        {
+            exluse = new List<Tile> { this };
+        }
+        else
+        {
+            exluse.Add(this);
+        }
+
+        foreach (var neighbours in Neighbours)
+        {
+            if (neighbours == null || exluse.Contains(neighbours) || neighbours.Item != Item) continue;
+            
+            result.AddRange(neighbours.GetConnectedTiles(exluse));
+        }
+
+        return result;
     }
 }
